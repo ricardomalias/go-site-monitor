7e5822f7a5dd49d26ca6d769f402da28030488ca
Ricardo Malias Aguiar <ricardomalias@gmail.com>
2021-09-16 12:14:52 -0300

    feat: add banner and monitoring

--- hello.go
+++ hello.go
@@ -2,36 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
-	"reflect"
 )
+
+var URL = "https://forleven.com"
 
 func main() {
 	displayIntroduction()
-	displayMenu()
-	option := readOption()
 
-	switch option {
-	case 1:
-		fmt.Println("Monitoring...")
-	case 2:
-		fmt.Println("Displaying logs")
-	case 0:
-		fmt.Println("Exiting...")
-		os.Exit(0)
-	default:
-		fmt.Println("Unknown option")
-		os.Exit(-1)
+	for {
+		displayMenu()
+		option := readOption()
+
+		switch option {
+		case 1:
+			startMonitor()
+		case 2:
+			fmt.Println("Displaying logs")
+		case 0:
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		default:
+			fmt.Println("Unknown option")
+			os.Exit(-1)
+		}
 	}
 }
 
 func displayIntroduction() {
 	name := "Abigobaldo"
 	version := 1.2
+	banner, _ := ioutil.ReadFile("./logo.txt")
 
+	fmt.Println(string(banner))
 	fmt.Println("Run ", name, " Run!")
 	fmt.Println("This program is on version: ", version)
-	fmt.Println(reflect.TypeOf(version))
 }
 
 func displayMenu() {
@@ -42,7 +49,26 @@ func displayMenu() {
 
 func readOption() int {
 	var option int
-	fmt.Scan(&option)
+	_, err := fmt.Scan(&option)
+	if err != nil {
+		return 0
+	}
 
 	return option
+}
+
+func startMonitor() *http.Response {
+	fmt.Println("Monitoring...")
+	response, err := http.Get(URL)
+	if err != nil {
+		return nil
+	}
+
+	if response.StatusCode == 200 {
+		fmt.Println("Site:", URL, "is online")
+	} else {
+		fmt.Println("Site:", URL, "is offline")
+	}
+
+	return response
 }
